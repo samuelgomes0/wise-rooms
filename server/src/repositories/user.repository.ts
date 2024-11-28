81b6432e2a3e8a7b6fe1f90188b8409da1b1bfb7
Samuel Gomes <samuel_gomes26@hotmail.com>
2024-11-28 02:21:14 -0300

    ✨ (user.repository.ts): adiciona a inclusão do campo 'role' nas consultas de usuários
    Essa mudança foi feita para permitir que as informações de papel dos usuários sejam retornadas nas consultas, melhorando a funcionalidade e a flexibilidade do repositório de usuários.

--- server/src/repositories/user.repository.ts
+++ server/src/repositories/user.repository.ts
@@ -10,6 +10,7 @@ export class UserRepository implements IUserRepository {
     return await prisma.user.findMany({
       include: {
         bookings: true,
+        role: true,
       },
     });
   }
